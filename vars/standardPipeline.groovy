8fc29cd4982e06f2abe5f4502ef75ed0e800e3ea
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-20 10:45:44 -0700

    checking url

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -13,19 +13,24 @@ def call(body) {
 			branch1 = scm.branches[0].name
 			sh "echo $branch"
 			sh "echo $branch1"
+			// helm_chart_url = ${config.helm_artifactory_url} + ${config.helm_chart_name}
+			helm-chart-url = ${config.helm_artifactory_url}
+			helm-chart-name = ${config.helm_chart_name}
+			
 			if (branch.startsWith("feature") || branch.startsWith("dev")) {
 					echo "Starts with Feature* or Dev"
 					stage('Checkout') {
 						checkout scm
 					}
-				testScanStages()
 				buildStages()
+				scanStages()
+				testStages()
 			}
 			if (branch.startsWith("dev")) {
 				echo "Dev Branch"
 				publishStages()
 			}
-			if (branch == config.release_branch) {
+			if (branch.startsWith("dev") || branch.startsWith("rel") || branch.startsWith("master")) {
 				echo "Release branch or Master"
 				deployStages()
 			}
@@ -38,13 +43,10 @@ def call(body) {
 
 def buildStages() {
 	stage("Build") {
-		echo "building..."
+		echo "build code..."
 	}
 }
-def testScanStages(){
-	stage("Code-Scan") {
-		echo("---- Scan Stage -----")
-	}
+def testStages(){
 	stage('Test') {
 		parallel 'static': {
 			sh "echo 'shell scripts to run static tests...'"
@@ -55,7 +57,12 @@ def testScanStages(){
 				'integration': {
 					sh "echo 'shell scripts to run integration tests...'"
 				}
-		}
+	}
+}
+def scanStages(){
+	stage("Code-Scan") {
+		echo("---- Scan Stage -----")
+	}
 }
 def publishStages(){
 	def publishers = [:]
@@ -66,31 +73,34 @@ def publishStages(){
 			stage("Publish Docker Image") {
 				echo "Publish Docker"
 			}
-
 	}
 	publishers["gcr"] = {
 		stage("Push Image to GCR") {
-			echo "Pushing image to GCR"
+			echo "Pushing docker image to GCR"
 		}
 	}
 	publishers["helm-chart"] = {
-			stage("Build Helm Chart") {
-				echo "Build Helm Chart"
-			}
+			//stage("Build Helm Chart") {
+			//	echo "Build Helm Chart"
+			// }
+		//read environment_namespace variable from jenkinsfile and then publish
 			stage("Publish Helm Chart") {
 				echo "Publish Helm Chart"
+				// use helm_chart_url
+				// <environment_namespace>-<Helm-chart-name>
 			}
 	}
 	parallel publishers
 }
 def deployStages() {
 	stage("Fetch-Helm-Chart") {
+		// get <environment_namespace>-<Helm-chart-name>
+		// fetch  helm_chart_url
+		//unzip tgz
 		echo "Fetching Helm chart from Helm Artifactory"
 	}
 	stage("Deploy-to-GKE") {
+		//run helm command
 		echo "Deploying Helm chart to GKE cluster"
 	}
 }
-
-
-
