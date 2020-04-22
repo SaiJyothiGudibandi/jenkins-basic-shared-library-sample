ee43c7ea522aec24bb19c33068feea345a21d6a8
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-21 23:03:22 -0700

    Add files 8

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -43,7 +43,7 @@ def call(Map config) {
 	    // Clean workspace before doing anything
 	    deleteDir()
 
-		def build_info = readYaml file: "resources/values.yaml"
+		def build_info = readYaml file: "values.yaml"
 
 	    try {
 			branch = env.BRANCH_NAME ? "${env.BRANCH_NAME}" : scm.branches[0].name
