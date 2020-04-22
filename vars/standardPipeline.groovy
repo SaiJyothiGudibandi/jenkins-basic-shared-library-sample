25d4c5248bfeccff4dbb81e8b6b18ebe6bcf2e60
SAIJYOTHI GUDIBANDI <saijyothi.gudibandi@broadcom.com>
2020-04-22 00:46:34 -0700

    Add files 16

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -138,7 +138,7 @@ def deployStages(helm_chart_url, helm_docker_img, branch) {
 def qualityGate(helm_docker_img, branch){
 	println helm_docker_img
 	//([^\/]+$)
-	def helm_docker_img_label = helm_docker_img.substring(helm_docker_img.lastIndexOf(":") + 1)
+	def helm_docker_img_label = helm_docker_img.substring(helm_docker_img.lastIndexOf("/") + 1)
 	println helm_docker_img_label
 	//if (helm_docker_img_label.startsWith("feature") && )
 	println branch
